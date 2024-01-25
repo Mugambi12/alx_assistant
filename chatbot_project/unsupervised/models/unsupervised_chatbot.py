29e2bec74e6df785dee2f761357a5b992c656c37
mugambi12 <silasmungiria16@gmail.com>
2024-01-25 10:06:09 +0300

    cleaning code

--- chatbot_project/unsupervised/models/unsupervised_chatbot.py
+++
@@ -1,8 +0,0 @@
-from models.responses import Responses
-
-class UnsupervisedChatbot:
-    def __init__(self):
-        self.responses = Responses()
-
-    def get_response(self, user_message):
-        return self.responses.get_response(user_message)
